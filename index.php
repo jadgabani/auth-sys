89ca7c31428adf64dab8a7f2b802a92942741d89
Merry <merrychrisd29@gmail.com>
2025-07-02 12:44:45 +0800

    Initial commint - PHP Auth sytem index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My App</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>Do you have an account?</p>
+    <a href="login.php">Yes, login now</a><br>
+    <p>Or do you need to create an account?</p>
+    <a href="register.php">Register now</a>
+</body>
+</html>
